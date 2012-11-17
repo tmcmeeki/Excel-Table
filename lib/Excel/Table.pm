545732e5449f96371164ab851f44976bdbac10a6
tmcmeeki <tmcmeeki@cpan.org>
2012-11-17 21:46:13 +1100

    Build V1.013

--- lib/Excel/Table.pm
+++ lib/Excel/Table.pm
@@ -209,7 +209,7 @@ use Log::Log4perl qw/ get_logger /;
 
 use vars qw/ @EXPORT $VERSION /;
 
-$VERSION = "1.012";	# update this on new release
+$VERSION = "1.013";	# update this on new release
 
 #@ISA = qw(Exporter);
 #@EXPORT = qw();
